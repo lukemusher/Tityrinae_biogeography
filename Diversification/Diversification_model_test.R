f189d9913e7856c4aac59d190c439d47447f1877
lukemusher <musherluke@gmail.com>
2019-02-10 18:26:10 -0500

    Update Diversification_model_test.R

--- Diversification/Diversification_model_test.R
+++ Diversification/Diversification_model_test.R
@@ -40,7 +40,6 @@ Model6<-fit_coal_var(tree, lamb0=0.34, alpha=-0.001, mu0=0.12, beta=0, N0=50, cs
 
 weights<-aicw(c(Model1$aicc, Model2$aicc, Model3$aicc, Model4a$aicc, Model4b$aicc, Model4c$aicc, Model4d$aicc, Model5$aicc, Model6$aicc))
 
-#environmental model selection in RPANDA: 
 #environmental model selection: first condition on crown
 #first models without envi variable
 ?fit_bd
@@ -53,6 +52,9 @@ mu_par<-c(0)
 
 BD_model1<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=T, cst.mu = T, fix.mu =T, cond = "crown")
 BD_model1
+plot_fit_bd(BD_model1, tot_time = 22.90199861)
+plot_dtt(BD_model1, tot_time = 22.90199861, N0 = 56)
+ltt.lines(tree, col="darkred", lwd=3)
 
 #birth-death constant rates
 f.lamb<-function(t,y){y[1]}
@@ -62,6 +64,9 @@ mu_par<-c(0.1)
 
 BD_model2<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=T, cst.mu = T, cond = "crown")
 BD_model2
+plot_fit_bd(BD_model2, tot_time = 22.90199861)
+plot_dtt(BD_model2, tot_time = 22.90199861, N0 = 56)
+ltt.lines(tree, col="darkred", lwd=3)
 
 #pure birth exponential dependence on time
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
@@ -72,6 +77,10 @@ mu_par<-c(0)
 BD_model3<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = F, fix.mu = T, cond = "crown", dt = 1e-3)
 BD_model3
 
+plot_fit_bd(BD_model3, tot_time = 22.90199861)
+plot_dtt(BD_model3, tot_time = 22.90199861, N0 = 56)
+ltt.lines(tree, col="darkred", lwd=3)
+
 #speciation exponential dependence on time with constant extinction
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
 f.mu<-function(t,y){y[1]}
@@ -80,6 +89,9 @@ mu_par<-c(0.1)
 
 BD_model4<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = T, cond = "crown")
 BD_model4
+plot_fit_bd(BD_model4, tot_time = 22.90199861)
+plot_dtt(BD_model4, tot_time = 22.90199861, N0 = 56)
+ltt.lines(tree, col="darkred", lwd=3)
 
 #birth-death exponential dependence on time
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
@@ -89,9 +101,13 @@ mu_par<-c(0.1,0.01)
 
 BD_model5<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = F, cond = "crown")
 BD_model5
+plot_fit_bd(BD_model5, tot_time = 22.90199861)
+
+plot_dtt(BD_model5, tot_time = 22.90199861, N0 = 56)
+ltt.lines(tree, col="darkred", lwd=3)
 
 #pure birth exponential dependence on temperature
-env_data<-("Zachos_climate.csv")
+env<-read.csv("Zachos_climate.csv")
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y}
 lamb_par<-c(0.3, 0.01)
@@ -102,7 +118,6 @@ Env_model1
 plot_fit_env(Env_model1, env, 22.90199861)
 
 #birth exponential dependence on temperature constant extinction
-
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y}
 lamb_par<-c(0.3, 0.01)
@@ -110,7 +125,6 @@ mu_par<-c(0.1)
 
 Env_model2<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "crown", dt = 1e-3, cst.mu = T,fix.mu = F, expo.lamb = T, expo.mu = T)
 Env_model2
-#plot_fit_env(Env_model2, env, 22.90199861)
 
 #exponential dependence of speciation and extinction on temperature
 
@@ -121,12 +135,12 @@ mu_par<-c(0.1,0.01)
 
 Env_model3<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "crown", dt = 1e-3, cst.lamb = F, cst.mu = F,fix.mu = F, expo.lamb = T, expo.mu = T)
 Env_model3
-plot_fit_env(Env_model2, env, 22.90199861)
+plot_fit_env(Env_model3, env, 22.90199861)
 
 AICs<-c(BD_model1$aicc, BD_model2$aicc, BD_model3$aicc, BD_model4$aicc, BD_model5$aicc, Env_model1$aicc, Env_model2$aicc, Env_model3$aicc)
 aic.w(AICs)
 
-#environmental model selection: next condition on stem
+#environmental model selection: first condition on stem
 #first models without envi variable
 
 #pure birth constant rate
@@ -175,13 +189,12 @@ BD_model5<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = 
 BD_model5
 
 #pure birth exponential dependence on temperature
-env_data<-("Zachos_climate.csv")
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y}
 lamb_par<-c(0.3, 0.01)
 mu_par<-c(0)
 
-Env_model1<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "stem", dt = 1e-3, cst.mu = T,fix.mu = T, expo.lamb = T, expo.mu = T)
+Env_model1<-fit_env(tree, env_data = env_data, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "stem", dt = 1e-3, cst.mu = T,fix.mu = T, expo.lamb = T, expo.mu = T)
 Env_model1
 plot_fit_env(Env_model1, env, 22.90199861)
 
@@ -209,6 +222,11 @@ plot_fit_env(Env_model2, env, 22.90199861)
 
 AICs<-c(BD_model1$aicc, BD_model2$aicc, BD_model3$aicc, BD_model4$aicc, BD_model5$aicc, Env_model1$aicc, Env_model2$aicc, Env_model3$aicc)
 aic.w(AICs)
+
+plot_dtt(BD_model3, tot_time = 22.90199861, N0 = 56)
+ltt.lines(tree, col="darkred", lwd=3)
+
+plot_dtt(Env_model1, tot_time = 22.90199861, N0 = 56)
 
 #DDD analyses based on condamine et al., 2018, biol letters.
 
