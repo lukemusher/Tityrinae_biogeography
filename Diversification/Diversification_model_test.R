467605ccb1f35577c60ad53a115f66492b906686
lukemusher <musherluke@gmail.com>
2019-02-14 12:16:50 -0500

    Update Diversification_model_test.R

--- Diversification/Diversification_model_test.R
+++ Diversification/Diversification_model_test.R
@@ -11,17 +11,6 @@ library(TESS)
 #Read your tree
 read.tree("Tree.tre")->tree
 
-#Plot your lineages through time
-ltt.plot(tree, log="y", col="blue", lwd=3, xlim=c(-40, 0))
-
-#simulate some trees under constant b&d and plot their LTTs with your LTT
-replicate(10000,rbdtree(birth=bd.km(tree), death=0.0001, Tmax =max(branching.times(tree)), BIRTH = NULL,
-                      DEATH = NULL, eps = 1e-6), F)->simmed
-class(simmed)<-"multiPhylo"
-
-mltt.plot(simmed, log="y", legend=F)
-ltt.lines(tree, col="darkred", lwd=3)
-
 #one-tailed t-test gamma-test to test if div rates decline through time
 gamStat(branching.times(tree))
 
@@ -53,20 +42,20 @@ BD_model2
 #pure birth exponential dependence on time
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
 f.mu<-function(t,y){y}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0)
 
 BD_model3<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = F, fix.mu = T, cond = "stem", dt = 1e-3)
 BD_model3
 
-#plot_fit_bd(BD_model3, tot_time = 22.90199861)
-#plot_dtt(BD_model3, tot_time = 22.90199861, N0 = 56)
-#ltt.lines(tree, col="darkred", lwd=3)
+plot_fit_bd(BD_model3, tot_time = 22.90199861)
+plot_dtt(BD_model3, tot_time = 22.90199861, N0 = 56)
+ltt.lines(tree, col="darkred", lwd=3)
 
 #speciation exponential dependence on time with constant extinction
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
 f.mu<-function(t,y){y[1]}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1)
 
 BD_model4<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = T, cond = "stem")
@@ -90,7 +79,7 @@ BD_model5
 #birth-death exponential dependence on time
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
 f.mu<-function(t,y){y[1]*exp(y[2]*t)}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1,0.01)
 
 BD_model6<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = F, cond = "stem")
@@ -100,21 +89,22 @@ BD_model6
 #ltt.lines(tree, col="darkred", lwd=3)
 
 #pure birth exponential dependence on temperature
-data("InfTemp") #read in RPANDA temp data
-env<-InfTemp 
+#env<-read.csv("Zachos_climate.csv")
+data("InfTemp")
+env<-InfTemp
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0)
 
 Env_model1<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "stem", dt = 1e-3, cst.mu = T,fix.mu = T, expo.lamb = T, expo.mu = T)
 Env_model1
-plot_fit_env(Env_model1, env, 22.90199861)
+#plot_fit_env(Env_model1, env, 22.90199861)
 
 #birth exponential dependence on temperature constant extinction
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1)
 
 Env_model2<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "stem", dt = 1e-3, cst.mu = T,fix.mu = F, expo.lamb = T, expo.mu = T)
@@ -135,7 +125,7 @@ Env_model3
 
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y[1]*exp(y[2]*x)}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1,0.01)
 
 Env_model4<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "stem", dt = 1e-3, cst.lamb = F, cst.mu = F,fix.mu = F, expo.lamb = T, expo.mu = T)
@@ -161,7 +151,7 @@ mu_par<-c(0)
 BD_model1<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=T, cst.mu = T, fix.mu =T, cond = "crown")
 BD_model1
 #plot_fit_bd(BD_model1, tot_time = 22.90199861)
-#plot_dtt(BD_model1, tot_time = 22.90199861, N0 = 56)
+plot_dtt(BD_model1, tot_time = 22.90199861, N0 = 56)
 #ltt.lines(tree, col="darkred", lwd=3)
 
 #birth-death constant rates
@@ -179,7 +169,7 @@ BD_model2
 #pure birth exponential dependence on time
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
 f.mu<-function(t,y){y}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0)
 
 BD_model3<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = F, fix.mu = T, cond = "crown", dt = 1e-3)
@@ -192,7 +182,7 @@ BD_model3
 #speciation exponential dependence on time with constant extinction
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
 f.mu<-function(t,y){y[1]}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1)
 
 BD_model4<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = T, cond = "crown")
@@ -216,7 +206,7 @@ BD_model5
 #birth-death exponential dependence on time
 f.lamb<-function(t,y){y[1]*exp(y[2]*t)}
 f.mu<-function(t,y){y[1]*exp(y[2]*t)}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1,0.01)
 
 BD_model6<-fit_bd(phylo = tree, tot_time = 22.90199861, f.lamb = f.lamb, f.mu = f.mu, lamb_par = lamb_par, mu_par = mu_par, f = 0.85, cst.lamb=F, cst.mu = F, cond = "crown")
@@ -228,7 +218,7 @@ BD_model6
 #pure birth exponential dependence on temperature
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0)
 
 Env_model1<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "crown", dt = 1e-3, cst.mu = T,fix.mu = T, expo.lamb = T, expo.mu = T)
@@ -238,7 +228,7 @@ Env_model1
 #birth exponential dependence on temperature constant extinction
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1)
 
 Env_model2<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "crown", dt = 1e-3, cst.mu = T,fix.mu = F, expo.lamb = T, expo.mu = T)
@@ -257,14 +247,24 @@ Env_model3
 
 f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
 f.mu<-function(t,x,y){y[1]*exp(y[2]*x)}
-lamb_par<-c(0.3, 0.01)
+lamb_par<-c(0.3, -0.1)
 mu_par<-c(0.1,0.01)
 
 Env_model4<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "crown", dt = 1e-3, cst.lamb = F, cst.mu = F,fix.mu = F, expo.lamb = T, expo.mu = T)
 Env_model4
-#plot_fit_env(Env_model3, env, 22.90199861)
 
-AICs<-c(BD_model3$aicc, 
+f.lamb <-function(t,x,y){y[1]*exp(y[2]*x)}
+f.mu<-function(t,x,y){y[1]*exp(y[2]*x)}
+lamb_par<-c(0.35, -0.06)
+mu_par<-c(0.0006,0.36)
+
+Env_model4<-fit_env(tree, env_data = env, f.lamb = f.lamb, f.mu = f.mu, f=0.85, tot_time = 22.90199861, lamb_par = lamb_par, mu_par = mu_par, cond = "crown", dt = 1e-3, cst.lamb = F, cst.mu = F,fix.mu = F, expo.lamb = T, expo.mu = T)
+Env_model4
+
+#plot_fit_env(Env_model4, env, 22.90199861)
+
+AICs<-c(BD_model1$aicc, BD_model2$aicc, BD_model3$aicc, 
         BD_model4$aicc, BD_model5$aicc, BD_model6$aicc, Env_model1$aicc, 
         Env_model2$aicc, Env_model3$aicc,Env_model4$aicc)
 aic.w(AICs)
+
